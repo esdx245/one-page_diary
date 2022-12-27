494c7babb2989c24f94337902fe9dffa62f18cdb
esdx245 <esdx245@khu.ac.kr>
2022-12-27 19:05:51 +0900

    make diary section

---
+++ src/DiaryEditor.js
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+
+const DiaryEditor = () => {
+    const [state, setState ] = useState({
+        author: "",
+        content: "",
+        emotionRate:1,
+    })
+    const handleChange = (e) => {
+        setState({
+            ...state,/*번저 이것을 선언하고 아래에 변경할 내용을 적어야함. 코드는 위-> 아래로 실행 */
+            [e.target.name]: e.target.value
+        })
+    }
+    const handleSubmit = () => {
+        console.log(state);
+    }
+
+    return <div className="DiaryEditor">
+        <h2>오늘의 일기</h2>
+        <div>{/*이름을 입력하는 부분 */}
+            <input
+                name='author'
+                value={state.author} 
+                onChange={handleChange} /*값이 변화하면 그 입력된 값을 author로 변화하는 함수, 이 함수를 사용하지 않으면 아무리 입력창에 입력해도 초기값에서 변경되지 않음*/
+            /> {/* 입력값을 처리해야하므로 state임 */}
+        </div>
+        <div>{/*일기 내용을 입력하는 부분 */}
+            <textarea
+                name='content'
+                value={state.content}
+                onChange={handleChange}
+            />
+        </div>
+        <div>{/*감정점수를  입력하는 부분 */}
+            <span>오늘의 감정 점수 : </span>
+            <select name='emotionRate' value={state.emotionRate} onChange={handleChange}> 
+                <option value={1}>1</option>
+                <option value={2}>2</option>
+                <option value={3}>3</option>
+                <option value={4}>4</option>
+                <option value={5}>5</option>
+            </select>
+        </div>
+        <div>{/*저장하는 부분 */}
+            <button onClick={handleSubmit}>일기 저장하기</button>
+        </div>
+    </div>;
+};
+
+export default DiaryEditor;
