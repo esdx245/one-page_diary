25725034c17dcc1892976448562ba62b7fcf14bf
esdx245 <esdx245@khu.ac.kr>
2023-01-10 17:07:22 +0900

    delete hompage on package.json

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import './App.css';
-import React, {useState, useRef, useEffect, useMemo} from 'react';
-import DiaryEditor from './DiaryEditor';
-import DiaryList from './DiaryList';
+import "./App.css";
+import React, { useState, useRef, useEffect, useMemo } from "react";
+import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
 // import LifeCycle from './LifeCycle';
 
 // const templist = [
@@ -25,7 +25,9 @@ function App() {
   const idCount = useRef(0);
 
   const getData = async () => {
-    const res = await fetch('https://jsonplaceholder.typicode.com/comments').then((res) => res.json());
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
     //console.log(res);
     const initData = res.slice(0, 20).map((it) => {
       return {
@@ -33,16 +35,15 @@ function App() {
         content: it.body,
         emotionRate: Math.floor(Math.random() * 5) + 1,
         create_date: new Date().getTime(),
-        id : idCount.current++,
-      }
-    })
+        id: idCount.current++,
+      };
+    });
     setData(initData);
-  }
+  };
 
   useEffect(() => {
     getData();
-  }, []
-  )
+  }, []);
 
   const addDiary = (author, content, emotionRate) => {
     const create_date = new Date().getTime();
@@ -51,30 +52,38 @@ function App() {
       content,
       emotionRate,
       create_date,
-      id : idCount.current++,
-    }
+      id: idCount.current++,
+    };
     setData([newdiary, ...data]);
-  }
+  };
 
   const removeDiary = (targetId) => {
     setData(data.filter((it) => it.id !== targetId));
     alert(`${targetId}번 일기가 삭제되었습니다.`);
-  }
+  };
 
   const editDiary = (targetId, newContent) => {
-    setData(data.map((it) => it.id === targetId ? {...it, content: newContent} : it));
-  }
+    setData(
+      data.map((it) =>
+        it.id === targetId ? { ...it, content: newContent } : it
+      )
+    );
+  };
 
-  const getDiaryAnalysis = useMemo(() => {
-    const goodCount = data.filter((it) => it.emotionRate >= 3).length;
-    const badCount = data.length - goodCount;
-    const goodRatio = (goodCount / data.length) * 100;
-    console.log(`데이터 분석 시작 / 좋은 일기 개수 ${goodCount}개 / 나쁜 일기 개수 ${badCount}개 / 좋은 일기 비율 ${goodRatio}%`);
-    return { goodCount, badCount, goodRatio };
-  },[data.length] // data.length가 변경될때만 실행된다. 또한 useMemo는 값을 return 받으므로 더이상 getDiaryAnalysis는 함수가 아닌 값이다.
+  const getDiaryAnalysis = useMemo(
+    () => {
+      const goodCount = data.filter((it) => it.emotionRate >= 3).length;
+      const badCount = data.length - goodCount;
+      const goodRatio = (goodCount / data.length) * 100;
+      console.log(
+        `데이터 분석 시작 / 좋은 일기 개수 ${goodCount}개 / 나쁜 일기 개수 ${badCount}개 / 좋은 일기 비율 ${goodRatio}%`
+      );
+      return { goodCount, badCount, goodRatio };
+    },
+    [data.length] // data.length가 변경될때만 실행된다. 또한 useMemo는 값을 return 받으므로 더이상 getDiaryAnalysis는 함수가 아닌 값이다.
   );
 
-  const {goodCount, badCount, goodRatio} = getDiaryAnalysis; // 이 코드는 처음 생성될때 1번 실행되고, data가 init된 이후 1번 실행되고, data가 변경될때마다 실행된다.
+  const { goodCount, badCount, goodRatio } = getDiaryAnalysis; // 이 코드는 처음 생성될때 1번 실행되고, data가 init된 이후 1번 실행되고, data가 변경될때마다 실행된다.
 
   return (
     <div className="App">
@@ -84,9 +93,15 @@ function App() {
       <div>기분 좋은 일기 개수 : {goodCount}</div>
       <div>기분 나쁜 일기 개수 : {badCount}</div>
       <div>기분 좋은 일기 비율 : {goodRatio}%</div>
-      <DiaryList diaryList={data} removeDiary={removeDiary} editDiary = {editDiary} />
+      <DiaryList
+        diaryList={data}
+        removeDiary={removeDiary}
+        editDiary={editDiary}
+      />
     </div>
   );
 }
 
 export default App;
+
+const change_data = () => {};
--- src/DiaryList.js
+++ src/DiaryList.js
@@ -1,20 +1,26 @@
 import DiaryItem from "./DiaryItem";
 
-const DiaryList = ({ removeDiary,diaryList,editDiary }) => {
-    return (<div className="DiaryList">
-        <h2>일기 리스트</h2>
-        <h4>{diaryList.length}개의 일기가 저장되어 있습니다.</h4>
-        <div>
-            {diaryList.map((it) => (
-                <DiaryItem removeDiary={removeDiary} key={it.id} editDiary = {editDiary} {...it} />
-            ))}
-        </div>
+const DiaryList = ({ removeDiary, diaryList, editDiary }) => {
+  return (
+    <div className="DiaryList">
+      <h2>일기 리스트</h2>
+      <h4>{diaryList.length}개의 일기가 저장되어 있습니다.</h4>
+      <div>
+        {diaryList.map((it) => (
+          <DiaryItem
+            removeDiary={removeDiary}
+            key={it.id}
+            editDiary={editDiary}
+            {...it}
+          />
+        ))}
+      </div>
     </div>
-    );
-}
+  );
+};
 
 DiaryList.defaultProps = {
-    diaryList: []
+  diaryList: [],
 };
 
 export default DiaryList;
